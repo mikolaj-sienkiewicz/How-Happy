73d25d7c548f9bce54bd19874f535d25b9a2421b
Martin Kearn <martin.kearn@microsoft.com>
2016-03-14 12:58:10 +0000

    Base file upload working

--- HowHappy-Web/Controllers/HomeController.cs
+++ HowHappy-Web/Controllers/HomeController.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace HowHappy_Web.Controllers
 {
     public class HomeController : Controller
     {
+        //_apiKey: Replace this with your own Project Oxford Emotion API key, please do not use my key. I include it here so you can get up and running quickly but you can get your own key for free at https://www.projectoxford.ai/emotion 
+        public const string _apiKey = "1dd1f4e23a5743139399788aa30a7153";
+
+        //_apiUrl: The base URL for the API. Find out what this is for other APIs via the API documentation
+        public const string _apiUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        // POST: Home/FileExample
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Result(IFormFile file)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                //setup HttpClient
+                httpClient.BaseAddress = new Uri(_apiUrl);
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+
+                //setup data object
+                HttpContent content = new StreamContent(file.OpenReadStream());
+                content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/octet-stream");
+
+                //make request
+                var response = await httpClient.PostAsync(_apiUrl, content);
+
+                //read response and write to view
+                var responseContent = await response.Content.ReadAsStringAsync();
+                ViewData["Result"] = responseContent;
+            }
+
             return View();
         }
 
