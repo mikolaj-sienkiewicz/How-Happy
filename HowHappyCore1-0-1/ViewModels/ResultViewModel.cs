b7c174fd3fb92747764bd2137e243f8faddb1eb1
Martin Kearn <martin.kearn@microsoft.com>
2016-09-26 21:29:18 +0100

    Ported to Core 1.0.1

---
+++ HowHappyCore1-0-1/ViewModels/ResultViewModel.cs
@@ -0,0 +1,22 @@
+﻿using HowHappy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HowHappy.ViewModels
+{
+    public class ResultViewModel
+    {
+        public  List<Face> Faces { get; set; }
+
+        public string Emotion { get; set; }
+
+        public SelectList Emotions { get; set; }
+
+        public string ThemeColour { get; set; }
+
+        public string FAEmotionClass { get; set; }
+    }
+}
